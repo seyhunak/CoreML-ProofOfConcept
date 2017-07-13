cd047a6ea3b6628c93b1673a1231b9bcf982635f
Seyhun Akyürek <seyhunak@gmail.com>
2017-07-13 20:58:05 +0300

    Initial Commit

---
+++ CoreML-ProofOfConcept/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML-ProofOfConcept
+//
+//  Created by Seyhun Akyürek on 13/07/2017.
+//  Copyright © 2017 seyhunak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
